ab650d0ee95b8effcdf5a3b8bf98aa5a5ef3db6d
Hassan Mansuri <hassanmansuri570@gmail.com>
2025-05-24 19:18:27 +0530

    jkh

--- src/app.py
+++ src/app.py
@@ -9,18 +9,35 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    print('--- Upload endpoint called: client connected ---')
     if 'file' not in request.files:
+        print('No file part in request')
         return 'No file part', 400
     file = request.files['file']
     if file.filename == '':
+        print('No selected file')
         return 'No selected file', 400
     file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+    print(f'File uploaded: {file.filename}')
     return 'OK', 200
 
-@app.route('/files', methods=['GET'])
-def list_files():
+@app.route('/api/files', methods=['GET'])
+def api_list_files():
     files = os.listdir(UPLOAD_FOLDER)
     return jsonify(files)
+
+@app.route('/files', methods=['GET'])
+def html_list_files():
+    files = os.listdir(UPLOAD_FOLDER)
+    success = request.args.get('success')
+    html = '<h2>Uploaded Files</h2>'
+    if success:
+        html += '<div style="color: green; font-weight: bold;">File uploaded successfully!</div>'
+    html += '<ul>'
+    for f in files:
+        html += f'<li><a href="/files/{f}" download>{f}</a></li>'
+    html += '</ul>'
+    return html
 
 @app.route('/files/<filename>', methods=['GET'])
 def get_file(filename):
